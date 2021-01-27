c839315ebc65e95748f937ff8347307d7993caba
Leonix <leonix@motd.ru>
2021-01-27 20:02:54 +0300

    SyncManager sends and receives input frames via rpc

--- sync/SyncManager.gd
+++ sync/SyncManager.gd
@@ -87,8 +87,15 @@ var SyncPeer = preload("res://sync/SyncPeer.tscn")
 # must exactly match sendtable on server.
 onready var input_sendtable = parse_input_map(InputMap)
 
+# Whether connection to server is currently active. Only maintained on Client.
+var _is_connected_to_server = false
+
 func _ready():
 	get_local_peer()
+	get_tree().connect("network_peer_connected", self, "_player_connected")
+	get_tree().connect("network_peer_disconnected", self, "_player_disconnected")
+	get_tree().connect("connected_to_server", self, "_i_connected")
+	get_tree().connect("server_disconnected", self, "_i_disconnected")
 
 func _process(_delta):
 	if first_process_since_physics_process and state_id_frac_to_integer_reduction > 0:
@@ -99,7 +106,7 @@ func is_server():
 	return get_tree().network_peer and get_tree().is_network_server()
 
 func is_client():
-	return get_tree().network_peer and not get_tree().is_network_server()
+	return get_tree().network_peer and _is_connected_to_server and not get_tree().is_network_server()
 
 func _physics_process(_delta):
 
@@ -123,27 +130,9 @@ func _physics_process(_delta):
 	# Client: send input frames to server if time has come
 	if is_client():
 		var time = OS.get_system_time_msecs()
-		if (time - _mtime_last_input_batch_sent) * input_sendrate >= 1.0:
+		if (time - _mtime_last_input_batch_sent) * input_sendrate * input_sendrate >= 1000.0*(input_sendrate-.5):
 			_mtime_last_input_batch_sent = time
 			send_input_batch()
-
-# Game code calls this
-# - on server for each client connected, providing peer_id;
-# - on client when connected to server, with no peer_id
-func client_connected(peer_id=null):
-	if is_server() and peer_id > 0:
-		var peer = SyncPeer.instance()
-		peer.name = str(peer_id)
-		self.add_child(peer)
-
-# Game code calls this 
-# - on server when existing client disconnects, providing peer_id;
-# - on client when disconnected from server, with no peer_id
-func client_disconnected(peer_id=null):
-	if is_server() and peer_id > 0:
-		var peer = get_node(str(peer_id))
-		if peer:
-			peer.queue_free()
 
 # Helper to initialize sendtable based on InputMap
 func parse_input_map(input_map):
@@ -187,9 +176,161 @@ func sample_input():
 	get_local_peer().storage.write(input_id, result)
 
 func send_input_batch():
-	var local_peer = get_local_peer()
-	if local_peer.storage.ready_to_read():
-		print(local_peer.storage.container) # !!!
+	var storage = get_local_peer().storage
+	if not storage.ready_to_read():
+		return # paranoid check
+
+	assert(is_client())
+	assert(storage.container.size() >= self.input_frames_min_batch, 'input_frames_min_batch can not be less than input_frames_history_size')
+
+	var frames = []
+	var first_input_id = storage.last_state_id - self.input_frames_min_batch + 1
+	for iid in range(first_input_id, storage.last_state_id+1):
+		var frame = storage.read(iid);
+		assert(frame is Dictionary, "Unexpected Input Frame from SyncPeer storage")
+		frames.append(frame.duplicate())
+	
+	var packed_batch = pack_input_batch(input_sendtable, frames)
+	#print('sending input batch first_input_id=%s ' % first_input_id, frames)
+	rpc('receive_input_batch', first_input_id, PoolIntArray(packed_batch[0]), PoolStringArray(packed_batch[1]), packed_batch[2])
+
+master func receive_input_batch(first_input_id: int, sendtable_ids: Array, node_paths: Array, values: Array):
+	var frames = parse_input_batch(input_sendtable, sendtable_ids, node_paths, values)
+	# !!!
+	#print('received input batch first_input_id=%s ' % first_input_id, frames)
+
+static func pack_input_batch(sendtable, frames):
+	#
+	# (1) All mentioned sendtable entries (one packed array of ints)
+	# (2) All mentioned SyncBases (one packed array of strings)
+	#  - contains NodePaths relative to `get_tree().current_scene`
+	#  - after each nodepath empty string is repeated for each additional 
+	#    property beyond first from this nodepath
+	# (3) Then Array (untyped) of zero or more Arrays (untyped), one for each state_id in batch.
+	#   - (3.1) All boolean values from sendtables (several ints)
+	#     May be missing entirely.
+	#   - (3.2) All float values from sendtables (one packed array of floats)
+	#     May be missing if no floats and no (3.3) are to be sent.
+	#   - (3.3) All SyncBase values (several entries, one per string in (2))
+	#     May be missing if no client-owned values are to be sent.
+	#
+
+	# Figure out which sendtable entries are to be sent.
+	# This prepares (1).
+	var sendtable_ids = []
+	var sendtable_id = 0
+	var sendtable_type_by_id = {}
+	for type in sendtable:
+		for action in sendtable[type]:
+			sendtable_id += 1
+			for frame in frames:
+				if frame[action] != 0:
+					sendtable_ids.append(sendtable_id)
+					sendtable_type_by_id[sendtable_id] = type
+					break
+	
+	# (2) is not supported yet
+	var node_paths = [] 
+	
+	# (3) pack values
+	var values = []
+	if sendtable_ids.size() > 0 or node_paths.size() > 0:
+		for frame in frames:
+			var value = []
+			var floats = []
+			sendtable_id = 0
+			for type in sendtable:
+				for action in sendtable[type]:
+					sendtable_id += 1
+					type = sendtable_type_by_id.get(sendtable_id)
+					if type != null:
+						if type == 'bool':
+							var v = bool(frame[action])
+							# !!! TODO pack several bools into each int to save traffic
+							value.append(v)
+						else:
+							assert(type == 'float', 'Unknown sendtable type %s' % type)
+							floats.append(float(frame[action]))
+					
+			# !!! TODO: pack client-owned properties
+			var client_owned = []
+			
+			if floats.size() > 0 or client_owned.size() > 0:
+				value.append(PoolRealArray(floats))
+			if client_owned.size() > 0:
+				value.append(client_owned)
+			values.append(value)
+	
+	return [sendtable_ids, node_paths, values]
+
+static func parse_input_batch(sendtable, sendtable_ids: Array, node_paths: Array, values: Array):
+	var sendtable_action_by_id = {}
+	for id in sendtable_ids:
+		sendtable_action_by_id[id] = null
+	
+	var sendtable_id = 0
+	var empty_frame = {}
+	for type in sendtable:
+		for action in sendtable[type]:
+			sendtable_id += 1
+			if type == 'bool':
+				empty_frame[action] = 0
+			else:
+				empty_frame[action] = 0.0
+			if sendtable_id in sendtable_action_by_id:
+				sendtable_action_by_id[sendtable_id] = action
+
+	var result = []
+	
+	# when no values came, we assume proper number of empty frames
+	if values.size() <= 0:
+		for __ in range(SyncManager.input_frames_min_batch-1):
+			result.append(empty_frame.duplicate())
+		result.append(empty_frame)
+		return result
+	
+	for value in values:
+		var bools = []
+		var floats = null
+		var client_owned = null
+		for v in value:
+			match typeof(v):
+				TYPE_BOOL:
+					bools.append(int(v))
+				TYPE_INT:
+					# !!! TODO: unpack several bools from int when implemented
+					bools.append(int(v))
+				_:
+					if floats == null:
+						assert((v is Array) or (v is PoolRealArray), 'Array of floats expected')
+						floats = v
+					else:
+						assert(v is Array, 'Array expected')
+						assert(client_owned == null, 'Too much data')
+						client_owned = v
+
+		var frame = empty_frame.duplicate()
+		var bool_i = 0
+		var float_i = 0
+		sendtable_id = 0
+		for type in sendtable:
+			for action in sendtable[type]:
+				sendtable_id += 1
+				if sendtable_id in sendtable_action_by_id:
+					if type == 'bool':
+						assert(bool_i < bools.size(), 'Not enough booleans in Input frame batch')
+						frame[action] = bools[bool_i]
+						bool_i += 1
+					else:
+						assert(type == 'float', 'Unknown sendtable type %s' % type)
+						assert(floats != null and float_i < floats.size(), 'Not enough floats in Input frame batch')
+						frame[action] = floats[float_i]
+						float_i += 1
+		result.append(frame)
+		assert((float_i == 0 and floats == null) or float_i == floats.size(), "Too much floats in input frame batch")
+		assert(node_paths.size() <= 0, "Sending Client-Owned Properties is not implemented yet") # !!!
+	return result
+	
 
 # Returns an object to read player's input through,
 # like a (limited) drop-in replacement of Godot's Input class.
@@ -200,7 +341,7 @@ func get_input_facade(peer_unique_id:int = 0):
 	var peer = get_node("%s/SyncInputFacade" % peer_unique_id)
 	return peer if peer else SyncInputFacade.FakeInputFacade.new()
 
-# Instantiated instances of SyncBase report here upon creation
+# Instances of SyncBase report here upon creation
 func SyncBase_created(_sb, _spawner=null):
 	pass # !!!
 
@@ -221,6 +362,25 @@ func get_local_peer():
 		self.add_child(local_peer)
 
 	return local_peer
+
+# Signals from scene tree networking
+func _player_connected(peer_id=null):
+	if is_server() and peer_id > 0:
+		var peer = SyncPeer.instance()
+		peer.name = str(peer_id)
+		self.add_child(peer)
+
+func _player_disconnected(peer_id=null):
+	if is_server() and peer_id > 0:
+		var peer = get_node(str(peer_id))
+		if peer:
+			peer.queue_free()
+
+func _i_connected():
+	_is_connected_to_server = true
+
+func _i_disconnected():
+	_is_connected_to_server = false
 
 # Getters and setters
 func get_state_id_frac():
---
+++ test/unit/test_SyncManager.gd
@@ -0,0 +1,118 @@
+extends "res://addons/gut/test.gd"
+
+var SyncManagerResource = null
+var obj = null
+
+var sendtable1 = {
+	'bool': ['bool_zzzz', 'bool_qqqq'],
+	'float': ['float_z', 'float_q']
+}
+var sendtable2 = {
+	'bool': [],
+	'float': ['float_z']
+}
+var sendtable3 = {
+	'bool': ['bool_zzzz'],
+	'float': []
+}
+
+var __input_frames_min_batch
+
+func before_all():
+	SyncManagerResource = load("res://sync/SyncManager.gd")
+	__input_frames_min_batch = SyncManager.input_frames_min_batch
+	
+func before_each():
+	obj = SyncManagerResource.new()
+
+func after_each():
+	obj.free()
+	obj = null
+	SyncManager.input_frames_min_batch = __input_frames_min_batch
+
+func after_all():
+	SyncManagerResource = null
+
+func test_frame_batcher_parser1():
+	var frames = [{
+		'bool_qqqq': 0,
+		'bool_zzzz': 1,
+		'float_q': 100.5,
+		'float_z': 0.0,
+	}, {
+		'bool_qqqq': 0,
+		'bool_zzzz': 0,
+		'float_q': 0.0,
+		'float_z': 0.0,
+	}]
+	var packed_batch = obj.pack_input_batch(sendtable1, frames)
+	var frames_out = obj.parse_input_batch(sendtable1, packed_batch[0], packed_batch[1], packed_batch[2])
+	assert_eq_deep(frames, frames_out)
+
+func test_frame_batcher_parser2():
+	var frames = [{
+		'bool_qqqq': 0,
+		'bool_zzzz': 0,
+		'float_q': 0.0,
+		'float_z': 0.0,
+	}, {
+		'bool_qqqq': 0,
+		'bool_zzzz': 0,
+		'float_q': 0.0,
+		'float_z': 0.0,
+	}]
+	SyncManager.input_frames_min_batch = frames.size()
+	var packed_batch = obj.pack_input_batch(sendtable1, frames)
+	assert_eq([], packed_batch[2])
+	var frames_out = obj.parse_input_batch(sendtable1, packed_batch[0], packed_batch[1], packed_batch[2])
+	assert_eq_deep(frames, frames_out)
+
+func test_frame_batcher_parser3():
+	var frames = [{
+		'float_z': 1.0,
+	}, {
+		'float_z': 2.0,
+	}, {
+		'float_z': 0.0,
+	}]
+	var packed_batch = obj.pack_input_batch(sendtable2, frames)
+	var frames_out = obj.parse_input_batch(sendtable2, packed_batch[0], packed_batch[1], packed_batch[2])
+	assert_eq_deep(frames, frames_out)
+
+func test_frame_batcher_parser4():
+	var frames = [{
+		'bool_zzzz': 1,
+	}, {
+		'bool_zzzz': 1,
+	}, {
+		'bool_zzzz': 0,
+	}]
+	var packed_batch = obj.pack_input_batch(sendtable3, frames)
+	var frames_out = obj.parse_input_batch(sendtable3, packed_batch[0], packed_batch[1], packed_batch[2])
+	assert_eq_deep(frames, frames_out)
+
+func test_frame_batcher_parser5():
+	var frames = [{
+		'float_z': 0.0,
+	}, {
+		'float_z': 0.0,
+	}, {
+		'float_z': 0.0,
+	}]
+	SyncManager.input_frames_min_batch = frames.size()
+	var packed_batch = obj.pack_input_batch(sendtable2, frames)
+	var frames_out = obj.parse_input_batch(sendtable2, packed_batch[0], packed_batch[1], packed_batch[2])
+	assert_eq_deep(frames, frames_out)
+
+func test_frame_batcher_parser6():
+	var frames = [{
+		'bool_zzzz': 0,
+	}, {
+		'bool_zzzz': 0,
+	}, {
+		'bool_zzzz': 0,
+	}]
+	SyncManager.input_frames_min_batch = frames.size()
+	var packed_batch = obj.pack_input_batch(sendtable3, frames)
+	var frames_out = obj.parse_input_batch(sendtable3, packed_batch[0], packed_batch[1], packed_batch[2])
+	assert_eq_deep(frames, frames_out)
